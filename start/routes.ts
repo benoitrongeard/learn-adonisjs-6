5ea3a8836aed0879fd09720db88874e0ed6356e5
Rongeard Benoit <benoit.rongeard@gmail.com>
2024-06-25 21:53:24 +0200

    2.7 Use markdown sub view and import them

--- start/routes.ts
+++ start/routes.ts
@@ -10,16 +10,21 @@
 import { Exception } from '@adonisjs/core/exceptions'
 import app from '@adonisjs/core/services/app'
 import router from '@adonisjs/core/services/router'
+import { MarkdownFile } from '@dimerapp/markdown'
+import { toHtml } from '@dimerapp/markdown/utils'
 import fs from 'node:fs/promises'
 
 router.on('/').render('pages/home').as('home')
 
 router
   .get('/movies/:slug', async (ctx) => {
-    const url = app.makeURL(`resources/movies/${ctx.params.slug}.html`)
+    const url = app.makeURL(`resources/movies/${ctx.params.slug}.md`)
 
     try {
-      const movie = await fs.readFile(url, 'utf8')
+      const file = await fs.readFile(url, 'utf8')
+      const md = new MarkdownFile(file)
+      await md.process()
+      const movie = toHtml(md).contents
       ctx.view.share({ movie })
     } catch (error) {
       throw new Exception(`Could not find a movie called ${ctx.params.slug}`, {
