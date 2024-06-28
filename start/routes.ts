41ed4f4f69ecc312321c13213c82b6aafec2f09f
Rongeard Benoit <benoit.rongeard@gmail.com>
2024-06-28 15:55:41 +0200

    2.10 Adding movie controller

--- start/routes.ts
+++ start/routes.ts
@@ -7,32 +7,12 @@
 |
 */
 
-import MovieService from '#services/movie_service'
 import router from '@adonisjs/core/services/router'
-import { toHtml } from '@dimerapp/markdown/utils'
+const MoviesController = () => import('#controllers/movies_controller')
+
+router.get('/', [MoviesController, 'index']).as('home')
 
 router
-  .get('/', async (ctx) => {
-    const slugs = await MovieService.getSlugs()
-    const movies: Record<string, any> = []
-    for (const slug of slugs) {
-      const md = await MovieService.read(slug)
-      movies.push({
-        title: md.frontmatter.title,
-        summary: md.frontmatter.summary,
-        slug,
-      })
-    }
-    return ctx.view.render('pages/home', { movies })
-  })
-  .as('home')
-
-router
-  .get('/movies/:slug', async (ctx) => {
-    const mdFile = await MovieService.read(ctx.params.slug)
-    const movie = toHtml(mdFile).contents
-    ctx.view.share({ movie })
-    return ctx.view.render('pages/movies/show')
-  })
+  .get('/movies/:slug', [MoviesController, 'show'])
   .as('movies.show')
   .where('slug', router.matchers.slug())
