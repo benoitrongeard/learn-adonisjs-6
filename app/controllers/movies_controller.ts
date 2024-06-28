1333956be17c620005d5fa913133196d9b3e35bc
Rongeard Benoit <benoit.rongeard@gmail.com>
2024-06-28 16:43:00 +0200

    2.11 Adding movie model

--- app/controllers/movies_controller.ts
+++ app/controllers/movies_controller.ts
@@ -1,25 +1,14 @@
-import MovieService from '#services/movie_service'
+import Movie from '#models/movie'
 import type { HttpContext } from '@adonisjs/core/http'
-import { toHtml } from '@dimerapp/markdown/utils'
 
 export default class MoviesController {
   async index({ view }: HttpContext) {
-    const slugs = await MovieService.getSlugs()
-    const movies: Record<string, any> = []
-    for (const slug of slugs) {
-      const md = await MovieService.read(slug)
-      movies.push({
-        title: md.frontmatter.title,
-        summary: md.frontmatter.summary,
-        slug,
-      })
-    }
+    const movies = await Movie.findAll()
     return view.render('pages/home', { movies })
   }
 
   async show({ view, params }: HttpContext) {
-    const mdFile = await MovieService.read(params.slug)
-    const movie = toHtml(mdFile).contents
+    const movie = await Movie.find(params.slug)
     return view.render('pages/movies/show', { movie })
   }
 }
---
+++ app/models/movie.ts
@@ -0,0 +1,45 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+import MovieService from '#services/movie_service'
+import { toHtml } from '@dimerapp/markdown/utils'
+
+// export default class Movie extends BaseModel {
+//   @column({ isPrimary: true })
+//   declare id: number
+
+//   @column.dateTime({ autoCreate: true })
+//   declare createdAt: DateTime
+
+//   @column.dateTime({ autoCreate: true, autoUpdate: true })
+//   declare updatedAt: DateTime
+// }
+
+export default class Movie {
+  declare title: string
+
+  declare slug: string
+
+  declare summary: string
+
+  declare abstract?: string
+
+  static async findAll() {
+    const slugs = await MovieService.getSlugs()
+    const movies: Movie[] = []
+    for (const slug of slugs) {
+      const movie = await Movie.find(slug)
+      movies.push(movie)
+    }
+    return movies
+  }
+
+  static async find(slug: string) {
+    const mdFile = await MovieService.read(slug)
+    const movie = new Movie()
+    movie.abstract = toHtml(mdFile).contents
+    movie.title = mdFile.frontmatter.title
+    movie.summary = mdFile.frontmatter.summary
+    movie.slug = slug
+    return movie
+  }
+}
